ef19046bdf14a370130c7de32811a34a99fc6e8c
Matthew Johnston <mlarash@mit.edu>
2021-06-25 16:14:40 +0700

    Remove erase and write methods from flash driver

--- src/FlashIAP.h
+++ src/FlashIAP.h
@@ -20,10 +20,6 @@ class FlashIAP
     // TODO: confirm thread safety
     bool read(uint32_t address, uint8_t* data, int32_t size, uint32_t timeout = 500);
 
-    // TODO: confirm thread safety
-    bool write(uint32_t address, const uint8_t* buffer, int32_t size, uint32_t timeout = 500);
-    bool erase(uint32_t address, int32_t size, uint32_t timeout = 4000);
-
   private:
     bool isAligned(uint32_t number, uint32_t alignment);
     bool isAlignedToPage(uint32_t address, uint32_t size);
